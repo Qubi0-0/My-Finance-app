8337854c10cf11094cd3ab55fa6f0cf10ceff073
Jakub Zeifert <kuba1102@gmail.com>
2024-10-21 18:28:27 +0200

    Refactor code to import all functions from the finance module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use finance::calculate_cost_est;
+use finance::*;
 use std::error::Error;
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
+    
 
     ui.on_request_change_cost_estimations({
         let ui_handle = ui.as_weak();
