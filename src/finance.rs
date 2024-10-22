5a8c62972f1fec544343f8c11d9e861286b497c9
Jakub Zeifert <kuba1102@gmail.com>
2024-10-22 12:33:45 +0200

    Refactor Cargo.toml and add dev-dependencies

--- src/finance.rs
+++ src/finance.rs
@@ -1,5 +1,23 @@
-use finance::calculate_cost_est;
 use slint::SharedString;
+
+pub fn calculate_cost_est(cost: f32, time_span: SharedString) -> (f32, f32, f32, f32) {
+    let time_span_str: &str = &time_span;
+    let (daily, weekly, monthly, yearly) = match time_span_str {
+        "Daily" => (cost, cost * 7.0, cost * 30.0, cost * 365.0),
+        "Weekly" => (cost / 7.0, cost, cost * 4.2857, cost * 52.1429),
+        "Monthly" => (cost / 30.0, cost / 4.2857, cost, cost * 12.0),
+        "Yearly" => (cost / 365.0, cost / 52.1429, cost / 12.0, cost),
+        _ => (0.0, 0.0, 0.0, 0.0), // Handle unexpected values
+    };
+
+    (
+        (daily * 100.0).round() / 100.0,
+        (weekly * 100.0).round() / 100.0,
+        (monthly * 100.0).round() / 100.0,
+        (yearly * 100.0).round() / 100.0,
+    )
+}
+
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use finance::*;
+mod finance;
+
+use finance::calculate_cost_est;
 use std::error::Error;
+use std::rc::Rc;
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = AppWindow::new()?;
-    
+pub struct State {
+    pub ui: AppWindow,
+    pub row_model: Rc<slint::VecModel<Row>>,
+}
+
+fn init() -> State {
+
+    let row_model = Rc::new(slint::VecModel::<Row>::from(vec![
+        Row { name: "test1".into(), value: 10.0, checked: true, timespan: "Daily".into() },
+        Row { name: "test2".into(), value: 15.0, checked: false, timespan: "Daily".into() },
+        Row { name: "test3".into(), value: 20.0, checked: true, timespan: "Daily".into() },
+    ]));
+
+    let ui = AppWindow::new().unwrap();
+
+    ui.on_add_element({
+        let row_model = row_model.clone();
+        println!("Element added!");
+        move |text, cost| row_model.push(Row { name: text, value: cost, checked: false, timespan: "Daily".into() })
+    });
 
     ui.on_request_change_cost_estimations({
         let ui_handle = ui.as_weak();
@@ -23,6 +43,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             ui.set_year_value(yearly);
         }
     });
+
+    State { ui, row_model }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    let state = init();
+    let ui = state.ui.clone_strong();
 
     ui.run()?;
 
