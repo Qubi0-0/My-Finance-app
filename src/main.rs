36c1ccf44dbb8e7a3eb27ecc492577437190b557
Jakub Zeifert <kuba1102@gmail.com>
2024-10-22 22:26:32 +0200

    It works, but still a lot of cleaning + ui change needed

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,51 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod finance;
+// mod finance;
+// use finance::calculate_cost_est;
+// use std::error::Error;
+// use std::rc::Rc;
+// slint::include_modules!();
 
-use finance::calculate_cost_est;
-use std::error::Error;
-use std::rc::Rc;
-slint::include_modules!();
+// pub struct State {
+//     pub ui: AppWindow,
+//     pub row_model: Rc<slint::VecModel<Row>>,
+// }
 
-pub struct State {
-    pub ui: AppWindow,
-    pub row_model: Rc<slint::VecModel<Row>>,
-}
+// fn init() -> State {
 
-fn init() -> State {
+//     let row_model = Rc::new(slint::VecModel::<Row>::from(vec![
+//         Row { name: "test1".into(), value: 10.0, checked: true, timespan: "Daily".into() },
+//         Row { name: "test2".into(), value: 15.0, checked: false, timespan: "Daily".into() },
+//         Row { name: "test3".into(), value: 20.0, checked: true, timespan: "Daily".into() },
+//     ]));
 
-    let row_model = Rc::new(slint::VecModel::<Row>::from(vec![
-        Row { name: "test1".into(), value: 10.0, checked: true, timespan: "Daily".into() },
-        Row { name: "test2".into(), value: 15.0, checked: false, timespan: "Daily".into() },
-        Row { name: "test3".into(), value: 20.0, checked: true, timespan: "Daily".into() },
-    ]));
+//     let ui = AppWindow::new().unwrap();
 
-    let ui = AppWindow::new().unwrap();
+//     ui.on_add_element({
+//         let row_model = row_model.clone();
+//         println!("Element added!");
+//         move |text, cost| row_model.push(Row { name: text, value: cost, checked: false, timespan: "Daily".into() })
+//     });
 
-    ui.on_add_element({
-        let row_model = row_model.clone();
-        println!("Element added!");
-        move |text, cost| row_model.push(Row { name: text, value: cost, checked: false, timespan: "Daily".into() })
-    });
+    // ui.on_request_change_cost_estimations({
+    //     let ui_handle = ui.as_weak();
+    //     move || {
+    //         let ui = ui_handle.unwrap();
+    //         let time_span = ui.get_selected_timespan();
+    //         let cost = ui.get_cost();
+    //         let (daily, weekly, monthly, yearly) = calculate_cost_est(cost, time_span);
 
-    ui.on_request_change_cost_estimations({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            let time_span = ui.get_selected_timespan();
-            let cost = ui.get_cost();
-            let (daily, weekly, monthly, yearly) = calculate_cost_est(cost, time_span);
+    //         ui.set_day_value(daily);
+    //         ui.set_week_value(weekly);
+    //         ui.set_month_value(monthly);
+    //         ui.set_year_value(yearly);
+    //     }
+    // });
 
-            ui.set_day_value(daily);
-            ui.set_week_value(weekly);
-            ui.set_month_value(monthly);
-            ui.set_year_value(yearly);
-        }
-    });
+//     State { ui, row_model }
+// }
 
-    State { ui, row_model }
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    
-    let state = init();
-    let ui = state.ui.clone_strong();
-
-    ui.run()?;
-
-    Ok(())
+fn main() {
+    todo_lib::main();
 }
---
+++ src/to_do.rs
@@ -0,0 +1,197 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: MIT
+mod finance;
+
+use finance::calculate_cost_est;
+use slint::{FilterModel, Model, SortModel};
+use std::rc::Rc;
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+slint::include_modules!();
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+pub fn main() {
+    let state = init();
+    let main_window = state.main_window.clone_strong();
+    #[cfg(target_os = "android")]
+    STATE.with(|main_window| *main_window.borrow_mut() = Some(state));
+    main_window.run().unwrap();
+}
+
+fn init() -> State {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(all(debug_assertions, target_arch = "wasm32"))]
+    console_error_panic_hook::set_once();
+
+    let todo_model = Rc::new(slint::VecModel::<TodoItem>::from(vec![
+        TodoItem { name: "test1".into(), value: 10.0, checked: true, timespan: "Daily".into() },
+        TodoItem { name: "test2".into(), value: 15.0, checked: false, timespan: "Daily".into() },
+        TodoItem { name: "test3".into(), value: 20.0, checked: true, timespan: "Daily".into() },
+    ]));
+
+    let main_window = AppWindow::new().unwrap();
+
+    main_window.on_todo_added({
+        let todo_model = todo_model.clone();
+        move |text| todo_model.push(TodoItem { name: text, value: 69.0, checked: false, timespan: "Daily".into() })
+    });
+    main_window.on_remove_done({
+        let todo_model = todo_model.clone();
+        move || {
+            let mut offset = 0;
+            for i in 0..todo_model.row_count() {
+                if todo_model.row_data(i - offset).unwrap().checked {
+                    todo_model.remove(i - offset);
+                    offset += 1;
+                }
+            }
+        }
+    });
+
+    main_window.on_request_change_cost_estimations({
+        let main_window_handle = main_window.as_weak();
+        move || {
+            let main_window = main_window_handle.unwrap();
+            let time_span = main_window.get_selected_timespan();
+            let cost = main_window.get_cost();
+            let (daily, weekly, monthly, yearly) = calculate_cost_est(cost, time_span);
+
+            main_window.set_day_value(daily);
+            main_window.set_week_value(weekly);
+            main_window.set_month_value(monthly);
+            main_window.set_year_value(yearly);
+        }
+    });
+
+    let weak_window = main_window.as_weak();
+    main_window.on_popup_confirmed(move || {
+        let window = weak_window.unwrap();
+        window.hide().unwrap();
+    });
+
+    main_window.on_apply_sorting_and_filtering({
+        let weak_window = main_window.as_weak();
+        let todo_model = todo_model.clone();
+
+        move || {
+            let window = weak_window.unwrap();
+            window.set_todo_model(todo_model.clone().into());
+
+            if window.get_hide_done_items() {
+                window.set_todo_model(
+                    Rc::new(FilterModel::new(window.get_todo_model(), |e| !e.checked)).into(),
+                );
+            }
+
+            if window.get_is_sort_by_name() {
+                window.set_todo_model(
+                    Rc::new(SortModel::new(window.get_todo_model(), |lhs, rhs| {
+                        lhs.name.to_lowercase().cmp(&rhs.name.to_lowercase())
+                    }))
+                    .into(),
+                );
+            }
+        }
+    });
+
+    main_window.set_show_header(true);
+    main_window.set_todo_model(todo_model.clone().into());
+    State { main_window, todo_model }
+}
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: slint::android::AndroidApp) {
+    use slint::android::android_activity::{MainEvent, PollEvent};
+    slint::android::init_with_event_listener(app, |event| {
+        match event {
+            PollEvent::Main(MainEvent::SaveState { saver, .. }) => {
+                STATE.with(|state| -> Option<()> {
+                    let todo_state = SerializedState::save(state.borrow().as_ref()?);
+                    saver.store(&serde_json::to_vec(&todo_state).ok()?);
+                    Some(())
+                });
+            }
+            PollEvent::Main(MainEvent::Resume { loader, .. }) => {
+                STATE.with(|state| -> Option<()> {
+                    let bytes: Vec<u8> = loader.load()?;
+                    let todo_state: SerializedState = serde_json::from_slice(&bytes).ok()?;
+                    todo_state.restore(state.borrow().as_ref()?);
+                    Some(())
+                });
+            }
+            _ => {}
+        };
+    })
+    .unwrap();
+    main();
+}
+
+pub struct State {
+    pub main_window: AppWindow,
+    pub todo_model: Rc<slint::VecModel<TodoItem>>,
+}
+
+#[cfg(target_os = "android")]
+thread_local! {
+    static STATE : core::cell::RefCell<Option<State>> = Default::default();
+}
+
+#[cfg(target_os = "android")]
+#[derive(serde::Serialize, serde::Deserialize)]
+struct SerializedState {
+    items: Vec<TodoItem>,
+    sort: bool,
+    hide_done: bool,
+}
+
+#[cfg(target_os = "android")]
+impl SerializedState {
+    fn restore(self, state: &State) {
+        state.todo_model.set_vec(self.items);
+        state.main_window.set_hide_done_items(self.hide_done);
+        state.main_window.set_is_sort_by_name(self.sort);
+        state.main_window.invoke_apply_sorting_and_filtering();
+    }
+    fn save(state: &State) -> Self {
+        Self {
+            items: state.todo_model.iter().collect(),
+            sort: state.main_window.get_is_sort_by_name(),
+            hide_done: state.main_window.get_hide_done_items(),
+        }
+    }
+}
+
+#[test]
+fn press_add_adds_one_todo() {
+    i_slint_backend_testing::init_no_event_loop();
+    use i_slint_backend_testing::{ElementHandle, ElementQuery};
+    let state = init();
+    state.todo_model.set_vec(vec![TodoItem { checked: false, title: "first".into() }]);
+    let line_edit = ElementQuery::from_root(&state.main_window)
+        .match_id("AppWindow::text-edit")
+        .find_first()
+        .unwrap();
+    assert_eq!(line_edit.accessible_value().unwrap(), "");
+    line_edit.set_accessible_value("second");
+
+    let button = ElementHandle::find_by_accessible_label(&state.main_window, "Add New Entry")
+        .next()
+        .unwrap();
+    button.invoke_accessible_default_action();
+
+    assert_eq!(state.todo_model.row_count(), 2);
+    assert_eq!(
+        state.todo_model.row_data(0).unwrap(),
+        TodoItem { checked: false, title: "first".into() }
+    );
+    assert_eq!(
+        state.todo_model.row_data(1).unwrap(),
+        TodoItem { checked: false, title: "second".into() }
+    );
+
+    assert_eq!(line_edit.accessible_value().unwrap(), "");
+}
