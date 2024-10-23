159ebb320e1a8eee12e65867c05e4e4885cfddfd
Jakub Zeifert <kuba1102@gmail.com>
2024-10-23 14:05:30 +0200

    Refactor code: Remove unnecessary files and directories

--- src/lib.rs
+++
@@ -1,2 +0,0 @@
-pub mod finance;
-pub mod add_new;
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-mod finance;
-mod add_new;
-fn main() {
-    add_new::main();
-}
